dbd7cca993ca70e57b28ff1839536477e73aa0fb
Chris Troutner <chris.troutner@gmail.com>
2020-08-11 21:45:53 -0700

    Simplifying component

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -7,46 +7,13 @@ class POToggle extends React.Component {
     super(props);
 
     _this = this;
-
-    this.state = {
-      address: "",
-      amountSat: "",
-      errMsg: "",
-      txId: "",
-      showScan: false,
-      inFetch: false,
-      postageRate: false,
-      postOffice: false,
-      postOfficeUrl: false,
-      merchantData: false,
-      postOfficeMsg: ""
-    };
   }
 
   render() {
     return (
-      <div style={{ marginBottom: "10px" }}>
-        <p>
-          Post Office{" "}
-          <strong>{_this.state.postOffice ? "Enabled" : "Disabled"}</strong>
-        </p>
-        <label
-          className="switch-address"
-          style={{ margin: "0 auto", display: "block" }}
-          htmlFor="address-checkbox"
-        >
-          <input
-            id="address-checkbox"
-            type="checkbox"
-            onChange={() =>
-              _this.setState({
-                postOffice: !_this.state.postOffice
-              })
-            }
-          />
-          <span className="slider round" />
-        </label>
-      </div>
+      <>
+        <h3>This is a plugin component</h3>
+      </>
     );
   }
 }
